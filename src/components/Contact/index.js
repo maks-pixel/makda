e28d696a753f6866ea3fede9d870d6d46038c206
Makda Girmay <makdagirmay4@gmail.com>
2022-03-19 15:17:41 -0400

    contact page submit and stuff

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,36 +1,44 @@
-import React from 'react';
-import Button from 'react-bootstrap/button';
-import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
 
 function ContactForm() {
- 
-  return (
-    <section>
-      <h1>Contact me</h1>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="name" placeholder="Enter Name" />
-          <Form.Text className="text-muted">
-            you gotta add you name here.
-          </Form.Text>
-          </Form.Group>
-          <Form.Group className="mb-3">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+  const [formState, setFormState] = useState({ name: '', email: '', message: ''});
+  const { name, email, message } = formState;
 
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Example textarea</Form.Label>
-          <Form.Control as="textarea" rows={3} />
-        </Form.Group>
-        <Button variant="secondary" type="submit">
-          Submit
-        </Button>
-      </Form>
+  function handleChange(e) {
+    setFormState({...formState, [e.target.name]: e.target.value })
+  }
+  
+  //console.log(formState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+  return (
+    <section className='contact-page'>
+      <h1>Contact me</h1>
+      <form id='contact-form' onSubmit={handleSubmit} >
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className='contact-links'>
+      <a href='https://github.com/maks-pixel' ><FontAwesomeIcon icon={faGithub} /></a>
+      <a href='https://www.linkedin.com/in/makda-girmay-a8b937b0/' ><FontAwesomeIcon icon={faLinkedin} /></a>
+      </div>
     </section>
   )
 }
