910d51f4f414a6962421d5eb157fe30c879d2d10
Makda Girmay <makdagirmay4@gmail.com>
2022-03-19 21:00:09 -0400

    making emails actually send

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
@@ -15,18 +16,27 @@ function ContactForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    emailjs.sendForm('service_0g40heo', 'template_5e7js4l', e.target, '9zTG0HzvdR-IKYNqg')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
     console.log(formState);
+    e.target.reset()
   }
   return (
-    <section className='contact-page'>
-      <h1>Contact me</h1>
+    <section className='contact'>  
+      <h1 className='header'>Contact me</h1>
+    <div className='contact-page'>
+      
       <form id='contact-form' onSubmit={handleSubmit} >
         <div>
           <label htmlFor="name">Name:</label>
           <input type="text" defaultValue={name} onChange={handleChange} name="name" />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
+          <label htmlFor="email">Email:</label>
           <input type="email" defaultValue={email} name="email" onChange={handleChange} />
         </div>
         <div>
@@ -36,9 +46,10 @@ function ContactForm() {
         <button type="submit">Submit</button>
       </form>
       <div className='contact-links'>
-      <a href='https://github.com/maks-pixel' ><FontAwesomeIcon icon={faGithub} /></a>
-      <a href='https://www.linkedin.com/in/makda-girmay-a8b937b0/' ><FontAwesomeIcon icon={faLinkedin} /></a>
+      <a href='https://github.com/maks-pixel' ><FontAwesomeIcon icon={faGithub} className='fa-4x'/></a>
+      <a href='https://www.linkedin.com/in/makda-girmay-a8b937b0/' ><FontAwesomeIcon icon={faLinkedin} className='fa-4x'/></a>
       </div>
+    </div>
     </section>
   )
 }
