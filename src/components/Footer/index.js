8159885482a5a76bdb1834449fd9269ef8fbd22f
Makda Girmay <makdagirmay4@gmail.com>
2022-03-19 21:34:25 -0400

    adding footer touches

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faGithub } from '@fortawesome/free-brands-svg-icons'; 
 
 function Footer() {
     return (
-        
-            <Navbar bg="dark" variant="dark" sticky="bottom">
-                <Navbar.Brand href="https://github.com/maks-pixel"><FontAwesomeIcon icon={faGithub} /></Navbar.Brand>
-                <Navbar.Toggle />
-                <Navbar.Collapse>
-                    <Navbar.Brand href="https://www.linkedin.com/in/makda-girmay-a8b937b0/"><FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon></Navbar.Brand>
-                </Navbar.Collapse>
-            </Navbar>
+        <section className='footer'>
+            <div className='footer-links'>
+                <a className='f-link' href='https://github.com/maks-pixel' ><FontAwesomeIcon icon={faGithub} className='fa-2x' /></a>
+                <a className='f-link' href='https://www.linkedin.com/in/makda-girmay-a8b937b0/' ><FontAwesomeIcon icon={faLinkedin} className='fa-2x' /></a>
+                <p className='f-link'> secret page </p>
+           </div>
+        </section>
     );
 }
 
