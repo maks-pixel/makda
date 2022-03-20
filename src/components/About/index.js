d7c528df7a259ea557eae3dc0e8d0cf7c984f59d
Makda Girmay <makdagirmay4@gmail.com>
2022-03-19 21:40:01 -0400

    deploy

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,7 @@ function About() {
                     <img className='image' src={require('../../assets/images/maks.jpg')} alt='creator'/>
                 </div>
                 <div className='bio'>
-                    <h3>Hi! my name is Maks</h3>
+                    <h3>Hi! my name is Makda (Maks) Girmay</h3>
                     <p>
                     I am a flegling Web Developer and all around nerd excited to learn more about web development and programming as a whole. Welcome to my First React site.
                         Here you will find some of my projects, my resume and a way to contact me. I am Looking forward to hearing form you.
