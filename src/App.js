9d203f0c959d37c949c5ef61ec074f1935ea3e7b
Makda Girmay <makdagirmay4@gmail.com>
2022-04-01 17:27:06 -0400

    creating a seperate home page from the about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ContactForm from "./components/Contact";
 import Header from "./components/Header";
+import Home from "./components/Home"
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Footer from "./components/Footer";
@@ -8,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
 
-  const [currentPage, handlePageChange] = useState('About');
+  const [currentPage, handlePageChange] = useState('Home');
 
   const renderPage = () => {
     switch (currentPage) {
@@ -16,8 +17,10 @@ function App() {
         return <Portfolio />;
       case 'Contact':
         return <ContactForm />;
+      case 'About':
+        return <About />;
       default:
-        return <About />
+        return <Home />
         ;
     }
   };
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function About() {
+function Home() {
     return (
         <section className='about'>
             <div className='about-container'>
@@ -8,9 +8,9 @@ function About() {
                     <img className='image' src={require('../../assets/images/maks.jpg')} alt='creator'/>
                 </div>
                 <div className='bio'>
-                    <h3>Hi! my name is Makda "Maks" Girmay</h3>
+                    <h3>Hi! My name is Makda "Maks" Girmay</h3>
                     <p>
-                    I am a junior Fads,gfDKFVBw;dfv;KADJFC;IASDHB;ISDBH JEDBFPWUEHF ISDHBFKJHS DVCJHD YIWDGVJBDSJHVGADJCVBASJHGLJWBEDFVLSDGVJLSDHVOUDFUYGVLSADBFLKjdbfs dihfemdxfcglwrg ull stack Web Developer and all around nerd, welcome to my portfolio.
+                    I am a junior Full stack Web Developer and all around nerd, welcome to my portfolio.
                         Here you will find a bit more about me, some of my projects, my resume and a few ways to contact me. I am Looking forward to hearing form you.
                     </p>
                 </div>
@@ -20,4 +20,4 @@ function About() {
     );
 }
 
-export default About;
+export default Home;
